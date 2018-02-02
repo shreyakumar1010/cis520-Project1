786d08b9de36f84854eb339770722b370bf0bdac
Bristol Miller <bristolnmiller@viper.cs.ksu.edu>
2018-02-02 10:47:54 -0600

    added check for ticks<=0

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -91,6 +91,9 @@ timer_sleep (int64_t ticks)
 {
   int64_t start = timer_ticks ();
 
+  if (ticks<=0)
+	  return;
+
   ASSERT (intr_get_level () == INTR_ON);
   while (timer_elapsed (start) < ticks) 
     thread_yield ();
