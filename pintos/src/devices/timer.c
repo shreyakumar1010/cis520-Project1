d1e20662351de38b52ad234a77fb5f3ccfcc3e8f
Bristol Miller <bristolnmiller@cougar.cs.ksu.edu>
2018-02-07 11:29:55 -0600

    lets hope this one works

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -93,8 +93,17 @@ void
 timer_sleep (int64_t ticks) 
 {
   int64_t start = timer_ticks ();
+  if (ticks<=0)
+	  return;
 
   ASSERT (intr_get_level () == INTR_ON);
+  
+  intr_disable();
+  list_insert_ordered(&timer_wait_list, &sleeperCells, less_wakeup, NULL);
+  intr_enable();
+
+  sema_down(thread_current-> timer_sema);
+
  /* while (timer_elapsed (start) < ticks) 
     thread_yield ();*/
 }
