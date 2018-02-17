c004bbd68c0b6aa24cd01d5343bc6c342daaa313
coreyvessar <coreyvessar@ksu.edu>
2018-02-17 13:23:33 -0600

    Update synch.c

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -219,7 +219,7 @@ void lock_acquire (struct lock *lock)
       thread_current()->waiting_for = lock;
       
       donate_priority(thread_current());   
-      list_insert_ordered(&lock->holder->list_of_priority_donations, &thread_current()-> donated_elem, (list_less_func*) & true_if_higher_priority, NULL);
+     // list_insert_ordered(&lock->holder->list_of_priority_donations, &thread_current()-> donated_elem, (list_less_func*) & true_if_higher_priority, NULL);
    }
 
   sema_down (&lock->semaphore);
