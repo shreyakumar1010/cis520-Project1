fd646fd7afb39ac21b39d84c2d166f6215ab5904
coreyvessar <coreyvessar@ksu.edu>
2018-03-12 16:04:48 -0500

    Update syscall.h

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -3,4 +3,24 @@
 
 void syscall_init (void);
 
+struct lock syscall_lock;
+
+struct process_file
+{
+  struct file *file;
+  int fd;
+  struct list_elem elem;
+};
+
+int process_add_file (struct file *f);
+struct file* process_get_file (int fd);
+
+static void syscall_handler (struct intr_frame *);
+int user_to_kernel_ptr(const void *vaddr);
+void get_arg (struct intr_frame *f, int *arg, int n);
+void check_valid_ptr (const void *vaddr);
+void check_valid_buffer (void* buffer, unsigned size);
+
+
+
 #endif /* userprog/syscall.h */
