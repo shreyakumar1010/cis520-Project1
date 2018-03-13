2480b27217321393f90d1b5ed93e738dadf85553
B Money <36802937+bristolmc@users.noreply.github.com>
2018-03-13 12:26:52 -0500

    Update thread.h

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -143,6 +143,7 @@ void thread_unblock (struct thread *);
 struct thread *thread_current (void);
 tid_t thread_tid (void);
 const char *thread_name (void);
+bool thread_alive(int pid);
 
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
