49e7b86186a393556f882a901d83db7375886dd4
B Money <36802937+bristolmc@users.noreply.github.com>
2018-03-14 12:30:46 -0500

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -46,8 +46,8 @@ process_execute (const char *file_name)
 
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
-   printf("%d", tid);
-   while(1);
+   //printf("%d", tid);
+   //while(1);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
