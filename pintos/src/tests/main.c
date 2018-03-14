0612e290ec489e3d85c2e8da05d636668fb6745c
B Money <36802937+bristolmc@users.noreply.github.com>
2018-03-14 17:13:35 -0500

    Update main.c

--- pintos/src/tests/main.c
+++ pintos/src/tests/main.c
@@ -5,9 +5,11 @@
 int
 main (int argc UNUSED, char *argv[]) 
 {
+  printf("main");
   test_name = argv[0];
-
+  printf("test_name= argv");
   msg ("begin");
+  printf("should have said begin ");
   random_init (0);
   test_main ();
   msg ("end");
