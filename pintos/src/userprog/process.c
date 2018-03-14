b66a9ffd1411a2bedbdce28fa680f2d723665370
coreyvessar <coreyvessar@ksu.edu>
2018-03-14 14:23:52 -0500

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -282,7 +282,7 @@ static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
 bool
 load (const char *file_name, void (**eip) (void), void **esp, char ** args) 
 {
-  ASSERT(false);
+  //ASSERT(false);
   struct thread *t = thread_current ();
   struct Elf32_Ehdr ehdr;
   struct file *file = NULL;
