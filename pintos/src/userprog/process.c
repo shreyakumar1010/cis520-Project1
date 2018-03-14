a758b3f9f935afc5fc6edc8236908accdcd65cba
coreyvessar <coreyvessar@ksu.edu>
2018-03-14 12:19:31 -0500

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -46,6 +46,8 @@ process_execute (const char *file_name)
 
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+   printf('%d', tid);
+   ASSERT(false);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
